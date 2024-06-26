1764d3b92ccfa5e2658c6575234129a05eb86464
eesha ali <eeshaali177@gmail.com>
2024-06-26 23:06:02 +0500

    eidt done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
+before_action :find_book, only: [:show, :edit, :update, :destroy ]
+
     def index
         @books = Book.all.order("created_at DESC")
     end
 
     def new 
         @book = Book.new
+    end 
+
+    def show 
     end 
 
     def create
@@ -15,10 +20,28 @@ class BooksController < ApplicationController
             render 'new'
         end
     end
+
+def edit
+end
+
+def update
+    if @book.update(book_params)
+        redirect_to book_path(@book)
+    else
+        render 'edit'
+    end
+end
+
+def destroy
+end
+
+
 private
 def book_params
     params.require(:book).permit(:title, :description, :author)
 end
-
+def find_book
+    @book = Book.find(params[:id])  
+end
 
 end
